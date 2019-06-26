dd6c78ed26de1ce89d5fb3c218c7d7c363ceb892
Jimmy Lafontaine Rivera <lafonj@gmail.com>
2019-06-26 18:01:28 -0400

    Fix CI scripts

--- ci_scripts/push_manifest.sh
+++ ci_scripts/push_manifest.sh
@@ -2,18 +2,18 @@ export IMAGE_TAG=$(cat VERSION)
 export DOCKER_CLI_EXPERIMENTAL=enabled
 
 
-$IMAGE_NAME=k8s-dns-dnsmasq
+IMAGE_NAME=k8s-dns-dnsmasq
 docker manifest create --amend cachengo/${IMAGE_NAME}:${IMAGE_TAG} cachengo/${IMAGE_NAME}-amd64:${IMAGE_TAG} cachengo/${IMAGE_NAME}-arm64:${IMAGE_TAG}
 docker manifest push cachengo/${IMAGE_NAME}:${IMAGE_TAG}
 
-$IMAGE_NAME=k8s-dns-sidecar
+IMAGE_NAME=k8s-dns-sidecar
 docker manifest create --amend cachengo/${IMAGE_NAME}:${IMAGE_TAG} cachengo/${IMAGE_NAME}-amd64:${IMAGE_TAG} cachengo/${IMAGE_NAME}-arm64:${IMAGE_TAG}
 docker manifest push cachengo/${IMAGE_NAME}:${IMAGE_TAG}
 
-$IMAGE_NAME=k8s-dns-kube-dns
+IMAGE_NAME=k8s-dns-kube-dns
 docker manifest create --amend cachengo/${IMAGE_NAME}:${IMAGE_TAG} cachengo/${IMAGE_NAME}-amd64:${IMAGE_TAG} cachengo/${IMAGE_NAME}-arm64:${IMAGE_TAG}
 docker manifest push cachengo/${IMAGE_NAME}:${IMAGE_TAG}
 
-$IMAGE_NAME=k8s-dns-dnsmasq-nanny
+IMAGE_NAME=k8s-dns-dnsmasq-nanny
 docker manifest create --amend cachengo/${IMAGE_NAME}:${IMAGE_TAG} cachengo/${IMAGE_NAME}-amd64:${IMAGE_TAG} cachengo/${IMAGE_NAME}-arm64:${IMAGE_TAG}
 docker manifest push cachengo/${IMAGE_NAME}:${IMAGE_TAG}
